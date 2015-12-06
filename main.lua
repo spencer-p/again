a32f4eeef7ec42c622861d2eca9fe2e87dc1b8ea
Spencer <spencerjpeterson@gmail.com>
2015-12-05 23:28:34 -0800

    factor player code into sep. file

--- main.lua
+++ main.lua
@@ -1,17 +1,10 @@
+require('purple')
+
 function love.load()
 	scale = 5
-	purple = { spritesheet = love.graphics.newImage("purple.png") }
-	purple.spritesheet:setFilter('nearest', 'nearest')
-	purple.still = love.graphics.newQuad(0, 0, 8, 8, 24, 8)
-	purple.stepLeft = love.graphics.newQuad(8, 0, 8, 8, 24, 8)
-	purple.stepRight = love.graphics.newQuad(16, 0, 8, 8, 24, 8)
-	purple.current = purple.still
-	purple.facing = 'right'
 	love.graphics.setBackgroundColor(32, 32, 32)
-	position = { x = love.graphics.getWidth()/(2*scale)-4, y = love.graphics.getHeight()/(2*scale)-4 }
-	velocity = { x = 0, y = 0 }
-	speed = 1
-	speedMultiplier = 1
+	player = Purple:new()
+	player:load()
 	counter = 0
 	tick = 0
 end
@@ -19,13 +12,9 @@ end
 function love.focus(f) running = f end
 
 function love.draw()
-	love.graphics.print(math.floor(counter).." "..love.timer.getFPS().."\n"..position.x..", "..position.y, 0, 0)
+	love.graphics.print(math.floor(counter).." "..love.timer.getFPS(), 0, 0)
 	love.graphics.scale(scale)
-	if purple.facing == 'right' then
-		love.graphics.draw(purple.spritesheet, purple.current, math.floor(position.x), math.floor(position.y))
-	else
-		love.graphics.draw(purple.spritesheet, purple.current, math.floor(position.x+8), math.floor(position.y), 0, -1, 1)
-	end
+	player:draw()
 end
 
 function love.update(dt)
@@ -36,68 +25,23 @@ function love.update(dt)
 	if tick < 2 then return end
 	tick = 0
 
-	counter = (counter + 120 * dt) % 120
-
-	if love.keyboard.isDown('up') then
-		position.y = position.y - speed*speedMultiplier
-		if position.y < 0 then
-			position.y = 0
-		end
-	end
-	if love.keyboard.isDown('down') then
-		position.y = position.y + speed*speedMultiplier
-		if position.y > love.graphics.getHeight()/scale-8 then
-			position.y = love.graphics.getHeight()/scale-8
-		end
-	end
-	if love.keyboard.isDown('left') then
-		position.x = position.x - speed*speedMultiplier
-		if position.x < 0 then
-			position.x = 0
-		end
-	end
-	if love.keyboard.isDown('right') then
-		position.x = position.x + speed*speedMultiplier
-		if position.x > love.graphics.getWidth()/scale-8 then
-			position.x = love.graphics.getWidth()/scale-8
-		end
-	end
-
-	if love.keyboard.isDown('up') or love.keyboard.isDown('down') or love.keyboard.isDown('left') or love.keyboard.isDown('right') then
-		if math.floor(counter/15) % 2 == 0 then
-			purple.current = purple.stepLeft
-		else
-			purple.current = purple.stepRight
-		end
-	else
-		purple.current = purple.still
-	end
+	player:update(dt)
 
 end
 
 function love.keypressed(key)
-	if key == 'left' or key == 'right' then
-		purple.facing = key
-	end
+
+	player:keypressed(key)
+
 	if key == 'escape' then
 		love.event.push('quit')
 	end
 	if key == ' ' then
 		running = not running
 	end
-	if key == 'lshift' then
-		speedMultiplier = 2
-	end
+
 end
 
 function love.keyreleased(key)
-	if key == purple.facing and (love.keyboard.isDown('left') or love.keyboard.isDown('right')) then
-		if key == 'left' then
-			purple.facing = 'right'
-		else
-			purple.facing = 'left'
-		end
-	elseif key == 'lshift' then
-		speedMultiplier = 1
-	end
+	player:keyreleased(key)
 end
---
+++ purple.lua
@@ -0,0 +1,104 @@
+Purple = {}
+
+function Purple:new()
+  local object = { src = { path = "purple.png", size = 8 } }
+  self.__index = self
+  return setmetatable(object, self)
+end
+
+function Purple:load()
+
+  -- load the sprite sheet
+  self.spritesheet = love.graphics.newImage(self.src.path)
+  self.spritesheet:setFilter('nearest', 'nearest')
+
+  -- set up the frames
+  self.still = love.graphics.newQuad(0, 0, self.src.size, self.src.size, 3*self.src.size, self.src.size)
+  self.stepLeft = love.graphics.newQuad(self.src.size, 0, self.src.size, self.src.size, 3*self.src.size, self.src.size)
+  self.stepRight = love.graphics.newQuad(2*self.src.size, 0, self.src.size, self.src.size, 3*self.src.size, self.src.size)
+  self.current = self.still
+
+  -- player's metadata
+  self.facing = "right"
+  self.pos = { x = love.graphics.getWidth()/(2*scale)-(self.src.size/2), y = love.graphics.getHeight()/(2*scale)-(self.src.size/2) }
+  self.velocity = { x = 0, y = 0 }
+  self.speed = 1
+
+end
+
+function Purple:draw()
+
+  -- scale -1 on x axis if facing left
+  if self.facing == "right" then
+    love.graphics.draw(self.spritesheet, self.current, math.floor(self.pos.x), math.floor(self.pos.y))
+  else
+    love.graphics.draw(self.spritesheet, self.current, math.floor(self.pos.x)+self.src.size, math.floor(self.pos.y), 0, -1, 1)
+  end
+
+end
+
+function Purple:update(dt)
+
+  -- input checks for movement
+
+  -- UP
+  if love.keyboard.isDown('up') then
+    self.pos.y = self.pos.y - self.speed
+    if self.pos.y < 0 then
+      self.pos.y = 0
+    end
+  end
+
+  -- DOWN
+  if love.keyboard.isDown('down') then
+    self.pos.y = self.pos.y + self.speed % (love.graphics.getHeight()/scale-self.src.size)
+  end
+
+  -- LEFT
+  if love.keyboard.isDown('left') then
+    self.pos.x = self.pos.x - self.speed
+    if self.pos.x < 0 then
+      self.pos.x = 0
+    end
+  end
+
+  -- RIGHT
+  if love.keyboard.isDown('right') then
+    self.pos.x = self.pos.x + self.speed % (love.graphics.getWidth()/scale-self.src.size)
+  end
+
+
+  -- decide which leg will be out
+  counter = (counter + 120 * dt) % 120 -- twice a second i think
+
+  if love.keyboard.isDown('up') or love.keyboard.isDown('down') or love.keyboard.isDown('left') or love.keyboard.isDown('right') then
+    if math.floor(counter/15) % 2 == 0 then
+      self.current = self.stepLeft
+    else
+      self.current = self.stepRight
+    end
+  else
+    self.current = self.still
+  end
+
+end
+
+function Purple:keypressed(key)
+  if key == 'left' or key == 'right' then
+    self.facing = key
+  elseif key == 'lshift' then
+    self.speed = 2
+  end
+end
+
+function Purple:keyreleased(key)
+  if key == self.facing and (love.keyboard.isDown('left') or love.keyboard.isDown('right')) then
+    if key == 'left' then
+      self.facing = 'right'
+    else
+      self.facing = 'left'
+    end
+  elseif key == 'lshift' then
+    self.speed = 1
+  end
+end
